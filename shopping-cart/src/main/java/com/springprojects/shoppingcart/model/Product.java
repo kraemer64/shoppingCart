425ae676d4cac467667af75b7a4378e35ee0809b
Daniel <54526557+kraemer64@users.noreply.github.com>
2025-01-30 19:27:09 +0100

    Added models
    Added:
    - Category
    - Image
    - Product

---
+++ shopping-cart/src/main/java/com/springprojects/shoppingcart/model/Product.java
@@ -0,0 +1,52 @@
+package com.springprojects.shoppingcart.model;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String brand;
+	private BigDecimal price;
+	private int inventory;
+	private String description;
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "category_id")
+	private Category category;
+	
+	/* mappedBy = "product" = Die Beziehung zwischen Product und Image wird 
+	 * 						  über Image.product verwaltet.
+	 * cascade = CascadeType.ALL = Alle Änderungen an Produkt, werden an den
+	 * 							   Produkten in images der Image Liste übertragen.
+	 * orphanRemoval = true = Wenn ein Bild aus der Liste entfernt wird, so wird
+	 * 						  auch das entsprechende Bild aus der DB gelöscht. Und
+	 * 						  wenn ein Produkt gelöscht wird, so werden alle 
+	 * 						  dazugehörigen Bilder gelöscht.
+	 * */
+	@OneToMany(mappedBy = "product", cascade = CascadeType.ALL,
+			orphanRemoval = true)
+	private List<Image> images;
+	
+}
