f2b001745e928aa451160bb60e7e0bcf73ff8e24
Daniel <54526557+kraemer64@users.noreply.github.com>
2025-02-01 16:59:24 +0100

    Update IProductService.java
    Added:
    - getProductsByCategoryAndBrand()
    - getProductsByName()
    - getProductsByBrandAndName()
    - countProductsByBrandAndName()

--- shopping-cart/src/main/java/service/product/IProductService.java
+++ shopping-cart/src/main/java/service/product/IProductService.java
@@ -13,4 +13,8 @@ public interface IProductService {
 	List<Product> getAllProducts();
 	List<Product> getProductsByCategory(String category);
 	List<Product> getProductsByBrand(String brand);
+	List<Product> getProductsByCategoryAndBrand(String category, String brand);
+	List<Product> getProductsByName(String name);
+	List<Product> getProductsByBrandAndName(String brand, String name);
+	Long countProductsByBrandAndName(String brand, String name);
 }
