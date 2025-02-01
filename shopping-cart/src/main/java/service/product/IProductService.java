3287a4862b2cabbb2af44da4152e6a29d06b64f4
Daniel <54526557+kraemer64@users.noreply.github.com>
2025-02-01 22:12:41 +0100

    Update IProductService.java
    Changed:
    - addProduct(AddProductRequest)

--- shopping-cart/src/main/java/service/product/IProductService.java
+++ shopping-cart/src/main/java/service/product/IProductService.java
@@ -3,10 +3,11 @@ package service.product;
 import java.util.List;
 
 import com.springprojects.shoppingcart.model.Product;
+import com.springprojects.shoppingcart.request.AddProductRequest;
 
 public interface IProductService {
 
-	Product addProduct(Product product);
+	Product addProduct(AddProductRequest product);
 	Product getProductById(Long id);
 	void deleteProductById(Long id);
 	void updateProduct(Product product, Long productId);
