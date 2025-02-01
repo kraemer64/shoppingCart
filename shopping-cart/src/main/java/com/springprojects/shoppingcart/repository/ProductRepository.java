d86385cea2fba900e012dffce54b7abaf7fefa83
Daniel <54526557+kraemer64@users.noreply.github.com>
2025-02-01 22:21:02 +0100

    Update ProductRepository.java
    Added methods:
    - findByCategoryName()
    - findByBrand()
    - findByCategoryNameAndBrand()
    - findByName()
    - findByBrandAndName()
    - countByBrandAndName()

--- shopping-cart/src/main/java/com/springprojects/shoppingcart/repository/ProductRepository.java
+++ shopping-cart/src/main/java/com/springprojects/shoppingcart/repository/ProductRepository.java
@@ -1,8 +1,16 @@
 package com.springprojects.shoppingcart.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.springprojects.shoppingcart.model.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long>{
 
+	List<Product> findByCategoryName(String name);
+	List<Product> findByBrand(String brand);
+	List<Product> findByCategoryNameAndBrand(String category, String brand);
+	List<Product> findByName(String name);
+	List<Product> findByBrandAndName(String brand, String name);
+	Long countByBrandAndName(String brand, String name);
 }
