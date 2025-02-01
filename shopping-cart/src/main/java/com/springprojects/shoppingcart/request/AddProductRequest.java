2de480372fc063468d4ce394f4c59930b0836095
Daniel <54526557+kraemer64@users.noreply.github.com>
2025-02-01 22:15:54 +0100

    Create AddProductRequest.java
    Created:
    - AddProduktRequest.java
    Class to create a product per service.

---
+++ shopping-cart/src/main/java/com/springprojects/shoppingcart/request/AddProductRequest.java
@@ -0,0 +1,19 @@
+package com.springprojects.shoppingcart.request;
+
+import java.math.BigDecimal;
+
+import com.springprojects.shoppingcart.model.Category;
+
+import lombok.Data;
+
+@Data
+public class AddProductRequest {
+
+	private Long id;
+	private String name;
+	private String brand;
+	private BigDecimal price;
+	private int inventory;
+	private String description;
+	private Category category;
+}
