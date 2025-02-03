6be29ab05d2dec3075b085def6fb271a48e16f1b
Daniel <54526557+kraemer64@users.noreply.github.com>
2025-02-03 17:46:10 +0100

    Maven version change and clean up
    - Updated Maven version after "Update Maven Project" Eclipse Bug.
    
    - Added @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class }) to prevent spring boot
    to configure a new db connection automatically.

--- shopping-cart/src/main/java/com/springprojects/shoppingcart/ShoppingCartApplication.java
+++ shopping-cart/src/main/java/com/springprojects/shoppingcart/ShoppingCartApplication.java
@@ -2,8 +2,9 @@ package com.springprojects.shoppingcart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class ShoppingCartApplication {
 
 	public static void main(String[] args) {
