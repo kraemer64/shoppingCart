89e77a1fd2b23b010a379465a59b2b512ddd1547
Daniel <54526557+kraemer64@users.noreply.github.com>
2025-02-04 20:02:39 +0100

    Create ICategoryService.java
    Created the interface for CategoryService.

---
+++ shopping-cart/src/main/java/service/category/ICategoryService.java
@@ -0,0 +1,15 @@
+package service.category;
+
+import java.util.List;
+
+import com.springprojects.shoppingcart.model.Category;
+
+public interface ICategoryService {
+
+	Category getCategoryById(Long id);
+	Category getCategoryByName(String name);
+	List<Category> getAllCategories();
+	Category addCategory(Category category);
+	Category updateCategory(Category category);
+	void deleteCategoryById(Long id);
+}
