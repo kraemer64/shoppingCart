ef7d139e96a05d1032ab00670b104cb78d6b2e20
Daniel <54526557+kraemer64@users.noreply.github.com>
2025-02-05 20:52:46 +0100

    Update CategoryRepository.java
    Added:
    - existsByName()

--- shopping-cart/src/main/java/com/springprojects/shoppingcart/repository/CategoryRepository.java
+++ shopping-cart/src/main/java/com/springprojects/shoppingcart/repository/CategoryRepository.java
@@ -7,5 +7,6 @@ import com.springprojects.shoppingcart.model.Category;
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
 	Category findByName(String name);
+	boolean existsByName(String name);
 
 }
