d1581510cddc4fed697c46c09a1e85e3d2a4ff80
Daniel <54526557+kraemer64@users.noreply.github.com>
2025-02-06 16:02:44 +0100

    Create ImageDto.java
    Created:
    - ImageDto.java

---
+++ shopping-cart/src/main/java/com/springprojects/shoppingcart/dto/ImageDto.java
@@ -0,0 +1,11 @@
+package com.springprojects.shoppingcart.dto;
+
+import lombok.Data;
+
+@Data
+public class ImageDto {
+
+	private Long imageId;
+	private String imageName;
+	private String downloadUrl;
+}
