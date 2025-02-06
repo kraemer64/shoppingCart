e2bff8569e7fe8156a6887fd2111cee0cfb03bcd
Daniel <54526557+kraemer64@users.noreply.github.com>
2025-02-06 16:01:52 +0100

    Create ApiResponse.java
    Created Api response for controller.

---
+++ shopping-cart/src/main/java/com/springprojects/shoppingcart/response/ApiResponse.java
@@ -0,0 +1,13 @@
+package com.springprojects.shoppingcart.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ApiResponse {
+
+	private String message;
+	private Object data;
+	
+}
