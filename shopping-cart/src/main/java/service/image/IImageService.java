878a74d00fdf8159af889b4a0efc3c562982f066
Daniel <54526557+kraemer64@users.noreply.github.com>
2025-02-06 16:01:26 +0100

    Update IImageService.java
    Added contracts to image service interface.
    
    Added:
    - getImageById()
    - deleteImageById()
    - saveImages()
    - updateImage()

--- shopping-cart/src/main/java/service/image/IImageService.java
+++ shopping-cart/src/main/java/service/image/IImageService.java
@@ -1,6 +1,16 @@
 package service.image;
 
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.springprojects.shoppingcart.dto.ImageDto;
+import com.springprojects.shoppingcart.model.Image;
+
 public interface IImageService {
 
-	
+	Image getImageById(Long id);
+	void deleteImageById(Long id);
+	List<ImageDto> saveImages(List<MultipartFile> file, Long productId);
+	void updateImage(MultipartFile file, Long imageId);
 }
