45f707b7dd4028f1953989c62e8fc137c4c843b9
Daniel <54526557+kraemer64@users.noreply.github.com>
2025-02-07 16:32:17 +0100

    Update ImageController.java
    Finished image controller by adding last endpoints.
    
    Added:
    - updateImage()
    - deleteImage()

--- shopping-cart/src/main/java/com/springprojects/shoppingcart/controller/ImageController.java
+++ shopping-cart/src/main/java/com/springprojects/shoppingcart/controller/ImageController.java
@@ -2,6 +2,7 @@ package com.springprojects.shoppingcart.controller;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
@@ -9,15 +10,19 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.springprojects.shoppingcart.dto.ImageDto;
+import com.springprojects.shoppingcart.exceptions.ResourceNotFoundException;
 import com.springprojects.shoppingcart.model.Image;
 import com.springprojects.shoppingcart.response.ApiResponse;
 
@@ -60,4 +65,40 @@ public class ImageController {
 						+ image.getFileName() + "\"")
 				.body(resource);
 	}
+	
+	@PutMapping("/image/{imageId}/update")
+	public ResponseEntity<ApiResponse> updateImage(@PathVariable final Long imageId, 
+			@RequestBody final MultipartFile file) {
+		try {
+			Optional<Image> imageOpt = Optional.ofNullable(imageService.getImageById(imageId));
+			if (imageOpt.isPresent()) {
+				imageService.updateImage(file, imageId);
+				return ResponseEntity.ok(new ApiResponse("Update success!", imageOpt.get()));
+			}
+		} catch (ResourceNotFoundException e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND)
+					.body(new ApiResponse(e.getMessage(), null));
+		}
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+				.body(new ApiResponse("Update failed!", HttpStatus.INTERNAL_SERVER_ERROR));
+	}
+	
+	@DeleteMapping("/image/{imageId}/delete")
+	public ResponseEntity<ApiResponse> deleteImage(@PathVariable final Long imageId) {
+		try {
+			Optional<Image> imageOpt = Optional.ofNullable(imageService.getImageById(imageId));
+			if (imageOpt.isPresent()) {
+				imageService.deleteImageById(imageId);
+				return ResponseEntity.ok(new ApiResponse("Delete success!", null));
+			}
+		} catch (ResourceNotFoundException e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND)
+					.body(new ApiResponse(e.getMessage(), null));
+		}
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+				.body(new ApiResponse("Delete failed!", HttpStatus.INTERNAL_SERVER_ERROR));
+	}
+	
+	
+	
 }
