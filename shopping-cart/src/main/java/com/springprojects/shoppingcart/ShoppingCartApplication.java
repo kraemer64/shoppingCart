9bc7de872fe64580b553798a17ae9c87f1e52425
Daniel <54526557+kraemer64@users.noreply.github.com>
2025-02-17 19:18:04 +0100

    Update ShoppingCartApplication.java
    Delete "exclude = {DataSourceAutoConfiguration.class}".
    
    This was added to prevent spring boot to use DataSourceAutoConfiguration for db connection, instead of the settings set at application.properties.
    
    Its deleted now, because it interfered with JPA and "entityManagerFactory" to find the needed repositories.

--- shopping-cart/src/main/java/com/springprojects/shoppingcart/ShoppingCartApplication.java
+++ shopping-cart/src/main/java/com/springprojects/shoppingcart/ShoppingCartApplication.java
@@ -3,8 +3,9 @@ package com.springprojects.shoppingcart;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class ShoppingCartApplication {
 
 	public static void main(String[] args) {
