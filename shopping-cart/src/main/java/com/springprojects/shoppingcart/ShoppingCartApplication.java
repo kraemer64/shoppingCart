b016b9d71133032c67a913267f377c8df84b8d20
Daniel <54526557+kraemer64@users.noreply.github.com>
2025-02-19 17:07:16 +0100

    Update ShoppingCartApplication.java
    Removed unused imports.

--- shopping-cart/src/main/java/com/springprojects/shoppingcart/ShoppingCartApplication.java
+++ shopping-cart/src/main/java/com/springprojects/shoppingcart/ShoppingCartApplication.java
@@ -2,8 +2,7 @@ package com.springprojects.shoppingcart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
 public class ShoppingCartApplication {
