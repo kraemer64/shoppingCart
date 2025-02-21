530b9ef26f78e04d8fc81f67812306e0242fd536
Daniel <54526557+kraemer64@users.noreply.github.com>
2025-02-21 19:15:21 +0100

    Update ImageRepository.java
    Added:
    - getImageByProductId()

--- shopping-cart/src/main/java/com/springprojects/shoppingcart/repository/ImageRepository.java
+++ shopping-cart/src/main/java/com/springprojects/shoppingcart/repository/ImageRepository.java
@@ -1,9 +1,10 @@
 package com.springprojects.shoppingcart.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.springprojects.shoppingcart.model.Image;
 
 public interface ImageRepository extends JpaRepository<Image, Long>{
 
+	List<Image> getImageByProductId(Long imageId);
 }
