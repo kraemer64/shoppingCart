2e2975ab6cb27fb968993f99b9a75b80f327d684
Daniel <54526557+kraemer64@users.noreply.github.com>
2025-03-05 19:49:11 +0100

    Update CartItemService.java
    Added:
    - Implementation of addItemToCart().

--- shopping-cart/src/main/java/com/springprojects/shoppingcart/service/cart/CartItemService.java
+++ shopping-cart/src/main/java/com/springprojects/shoppingcart/service/cart/CartItemService.java
@@ -2,7 +2,11 @@ package com.springprojects.shoppingcart.service.cart;
 
 import org.springframework.stereotype.Service;
 
+import com.springprojects.shoppingcart.model.Cart;
+import com.springprojects.shoppingcart.model.CartItem;
+import com.springprojects.shoppingcart.model.Product;
 import com.springprojects.shoppingcart.repository.CartItemRepository;
+import com.springprojects.shoppingcart.repository.CartRepository;
 import com.springprojects.shoppingcart.service.product.IProductService;
 
 import lombok.RequiredArgsConstructor;
@@ -13,11 +17,31 @@ public class CartItemService implements ICartItemService {
 	
 	private final CartItemRepository cartItemRepository;
 	private final IProductService productService;
+	private final ICartService cartService;
+	private final CartRepository cartRepository;
 
 	@Override
 	public void addItemToCart(final Long cartId, final Long productId, 
 			final int quantity) {
+		Cart cart = cartService.getCart(cartId);
+		Product product = productService.getProductById(productId);
+		CartItem cartItem = cart.getCartItems().stream()
+				.filter(item -> item.getProduct().getId().equals(productId))
+				.findFirst().orElse(new CartItem());
+		
+		if (cartItem.getCartItemId() == null) {
+			cartItem.setCart(cart);
+			cartItem.setProduct(product);
+			cartItem.setQuantity(quantity);
+			cartItem.setUnitPrice(product.getPrice());
+		} else {
+			cartItem.setQuantity(cartItem.getQuantity() + quantity);
+		}
 	
+		cartItem.setTotalPrice();
+		cart.addItem(cartItem);
+		cartItemRepository.save(cartItem);
+		cartRepository.save(cart);
 	}
 
 	@Override
